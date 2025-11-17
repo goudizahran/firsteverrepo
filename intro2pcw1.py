bf387447b7cc65cfc8f2b90b776433fbcb1b5643
goudi zahran <judyzahran7@gmail.com>
2025-11-17 22:02:39 +0200

    Update intro2pcw1.py

--- intro2pcw1.py
+++ intro2pcw1.py
@@ -1,1 +1,48 @@
-#intro 2 programming coursework 1 
+# Intro To Programming Coursework 1 
+# Steganography Program 
+# Goudi Zahran 202400725 
+
+
+
+START_MARKER = "$hhh"
+END_MARKER = "$ecret"
+
+
+
+# basic file functions 
+
+
+
+def open_image_file(filename):
+# attempt to open file and return its bytes
+    try:
+        with open(filename, "rb") as f:
+            return bytearray(f.read())
+    except:
+        print("file not found. please check file name")
+        return None 
+
+
+
+def save_image_file(filename, data):
+# save modified bytes to a new BMP file 
+    with open(filename, "wb") as f:
+        f.write(data)
+
+
+
+def is_bmp_file(data):
+# check BMP signature (first two bytes 'BM'), making sure this is a BMP image 
+    return data is not None and len(data) >= 2 and data[0:2] == b'BM'
+
+
+
+def get_pixel_data_offset(data):
+# byte offset where pixel array starts (from BMP header), this is done to determine where the pixels begin, clarifies where to hide the message and ensures the header is intact 
+    return int.from_bytes(data[10:14], byteorder='little')
+
+
+def get_bits_per_pixel(data):
+# return bits per pixel value from BMP header (for simple validation), determines whether the image is grey-scale or color (RGB)
+    return int.from_bytes(data[28:30], byteorder='little')
+
